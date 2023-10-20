65e006fa4bd1370c3116d09654e34b5262f2e38d
lfbutad <lfb@utad.pt>
2023-10-20 14:19:39 +0100

    1st part tutorial class6 implementation

---
+++ Class06/Data/DbInitializer.cs
@@ -0,0 +1,56 @@
+﻿using Class06.Models;
+
+namespace Class06.Data
+{
+    public class DbInitializer
+    {
+        private readonly Class06Context _context;
+
+        public DbInitializer(Class06Context context) { _context = context; }
+
+        public void Run()
+        {
+            _context.Database.EnsureCreated();
+
+            // Look for any categories.
+            if (_context.Students.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            Dictionary<string, List<Student>> collection = new Dictionary<string, List<Student>>();
+
+            using (StreamReader sr = File.OpenText("Data\\StudentsList_Class06.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(";"); // splits the information "number;name;class"
+
+                    if (collection.ContainsKey(parts[2]) == false) // creates a Dictionary with course as Key...
+                        collection.Add(parts[2], new List<Student>());
+                    // ... and list of students as Value
+                    collection[parts[2]].Add(new Student { Number = Convert.ToInt32(parts[0]), Name = parts[1] });
+                }
+
+                foreach (string _class in collection.Keys) // create Classes in Database
+                {
+                    _context.Classes.Add(new Class { Name = _class });
+                }
+                _context.SaveChanges();
+
+                foreach (KeyValuePair<string, List<Student>> aux in collection) // create Students in Database
+                {
+                    foreach (Student s in aux.Value)
+                    {
+                        s.ClassId = _context.Classes.Single(classes => classes.Name == aux.Key).Id;
+                        _context.Students.Add(s);
+                    }
+                }
+                _context.SaveChanges();
+            }
+
+
+        }
+    }
+}
---
+++ Class06/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Class06.Models;
+
+[Table("student")]
+public partial class Student
+{
+    [Key]
+    [Column("number")]
+    public int Number { get; set; }
+
+    [Column("name")]
+    [StringLength(200)]
+    public string Name { get; set; } = null!;
+
+    [Column("classId")]
+    public int? ClassId { get; set; }
+
+    [ForeignKey("ClassId")]
+    [InverseProperty("Students")]
+    public virtual Class? Class { get; set; }
+}
